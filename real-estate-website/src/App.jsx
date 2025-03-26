faa333d01236d0acfb81117f6bc1d3fa167d9c03
Muhammad Ali Khalil <mdalikhalil20@gmail.com>
2025-03-26 23:06:41 +0500

    Remove Unnecessary Terminators

--- real-estate-website/src/App.jsx
+++ real-estate-website/src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { ToastContainer } from 'react-toastify';
-import Header from "./components/Header";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import React from "react"
+import { ToastContainer } from 'react-toastify'
+import Header from "./components/Header"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Testimonials from "./components/Testimonials"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
